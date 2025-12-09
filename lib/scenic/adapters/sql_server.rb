a29498d8a5e87f3f465238b1635089195be7799f
Jonah George <jonah.george@icloud.com>
2025-12-08 23:15:14 -0800

    Reset extraneous files

--- lib/scenic/adapters/sql_server.rb
+++ lib/scenic/adapters/sql_server.rb
@@ -1,15 +1,15 @@
 module Scenic
   module Adapters
-
+    
     class SqlServer
       def initialize(connectable = ActiveRecord::Base)
         @connectable = connectable
       end
-
+    
       def views
          Views.new(connection).all
       end
-
+      
       def create_view(name, sql_definition)
         execute "CREATE VIEW #{quote_table_name(name)} AS #{sql_definition};"
       end
@@ -22,13 +22,13 @@ module Scenic
       def drop_view(name)
         execute "IF OBJECT_ID('#{quote_table_name(name)}') IS NOT NULL DROP VIEW #{quote_table_name(name)};"
       end
-
+      
       class NotSupportedError < StandardError
         def initialize
           super 'SQL Server does not support this feature'
         end
       end
-
+      
       %i[
         replace_view
         create_materialized_view
@@ -40,16 +40,16 @@ module Scenic
           raise NotSupportedError
         end
       end
-
+      
       private
-
+      
       attr_reader :connectable
       delegate :execute, :quote_table_name, to: :connection
-
+      
       def connection
         connectable.connection
       end
     end
-
+    
   end
 end
--- spec/scenic/adapters/sql_server/views_spec.rb
+++ spec/scenic/adapters/sql_server/views_spec.rb
@@ -1,5 +1,3 @@
-require "spec_helper"
-
 module Scenic
   module Adapters
     
